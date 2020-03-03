8d08a42b6434c6b1ee9166382fc611474ddd5246
Chris DeSilva <chris@skills.fund>
2020-03-03 15:00:12 -0600

    Tuition and COL updates populated by metro, reset to index 0 when program changes

--- src/utils/calculator.js
+++ src/utils/calculator.js
@@ -1,14 +1,18 @@
-export const calculateInterest = loanValue => {
-  let interest36 = Number(loanValue) * ((1.04 / 12) * 8.99) // based on 4% orig fee and 8.99% interest rate
-  let interest60 = Number(loanValue) * ((1.04 / 12) * 10.99) // based on 4% orig fee and 10.99 interest rate
-  return [interest36, interest60]
+export const calculateInterestPayment = (loanValue, setInterestPayments) => {
+  let interest36 = Number(loanValue) * ((1.04 / 12) * 0.0899) // based on 4% orig fee and 8.99% interest rate
+  let interest60 = Number(loanValue) * ((1.04 / 12) * 0.1099) // based on 4% orig fee and 10.99 interest rate
+  setInterestPayments({
+    payment36: Number(interest36.toFixed(2)),
+    payment60: Number(interest60.toFixed(2)),
+  })
 }
 
 export const calculateTotalPayment = (
   loanType,
   nonPaymentPeriod,
   interestPayment,
-  monthlyPayment
+  monthlyPayment,
+  setTotalPayments
 ) => {
   let months = [36, 60]
   let interestPeriod = nonPaymentPeriod
@@ -20,18 +24,26 @@ export const calculateTotalPayment = (
     payments[1] =
       interestPayment.payment60 * interestPeriod +
       monthlyPayment.payment60 * months[1]
+    setTotalPayments({
+      payment36: Number(payments[0].toFixed(2)),
+      payment60: Number(payments[1].toFixed(2)),
+    })
   } else if (loanType === "Immediate Repayment") {
     payments[0] = monthlyPayment.payment36 * months[0]
     payments[1] = monthlyPayment.payment60 * months[1]
+    setTotalPayments({
+      payment36: Number(payments[0].toFixed(2)),
+      payment60: Number(payments[1].toFixed(2)),
+    })
   }
-  return payments
 }
 
 export const calculateMonthlyPayment = (
   loanValue,
   defaultLoanAmount,
-  calcInterestFunc = calculateInterest,
-  calcTotalFunc
+  setMonthlyPayments
+  //   calcInterestFunc = calculateInterest,
+  //   calcTotalFunc = calculateTotalPayment
 ) => {
   const monthlyRate36 = 0.0899 / 12 // 8.99 interest rate for 36 months
   const monthlyRate60 = 0.1099 / 12 // 10.99 interest rate for 60 months
@@ -43,7 +55,10 @@ export const calculateMonthlyPayment = (
   let payment60 =
     Number(monthlyRate60 * totalLoan) /
     (1 - 1 / Math.pow(1 + monthlyRate60, 60))
-  return [payment36, payment60]
-  calcInterestFunc()
-  calcTotalFunc()
+  setMonthlyPayments({
+    payment36: Number(payment36.toFixed(2)),
+    payment60: Number(payment60.toFixed(2)),
+  })
+  //   calcInterestFunc()
+  //   calcTotalFunc()
 }
